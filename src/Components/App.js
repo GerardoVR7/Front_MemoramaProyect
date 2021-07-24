b4a586c3fc108774a5b4d8b134db03be3d30ca79
GerardoVR7 <GvrZero_07@outlook.com>
2021-07-23 23:44:39 -0500

    primera conexion con el servidor

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,9 +3,12 @@ import Header from './Header';
 import Tablero from './Tablero';
 import '../Assets/App.css';
 import construirBaraja from '../utils/construirBaraja';
+import { io } from 'socket.io-client'
+
 
 const getEstadoInicial = () => {
     const baraja = construirBaraja();
+
     return {
         baraja,
         parejaSeleccionada: [],
@@ -14,10 +17,30 @@ const getEstadoInicial = () => {
     };
 }
 
+
+
 class App extends Component {
+
     constructor(props) {
         super(props);
         this.state = getEstadoInicial();
+    }
+    componentDidMount(){
+        const socket = io.connect('ws://localhost:3000')  //'ws://localhost:3000'
+        socket.on('connection', (data) =>{
+            console.log(data)
+        })
+
+        /*socket.on('deck' , (data) => {
+            const baraja2 = data
+            this.handleDeck(baraja2)
+            console.log(baraja2)
+        } )*/
+
+    }
+
+    handleDeck (deck) {
+        this.setState({baraja: deck})
     }
 
     render() {
